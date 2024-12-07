cfeace9b009efa5b8e8acc08805799d4fe90bdf2
Ded2ec <ded2ec@Ded2ecs-iMac.local>
2024-12-07 22:23:43 +0900

    add pages&routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import { Route,Routes,BrowserRouter } from "react-router-dom"
+import Home from "@/pages/frontend/Home"
+import About from "@/pages/frontend/About"
+import Project from "@/pages/frontend/Project"
+import Team from "@/pages/frontend/Team"
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-green-700">
-    Hello world!
-  </h1>
+
+
+    <BrowserRouter>
+  
+  <Routes>
+    <Route>
+
+     <Route path="/" element={<Home />}/>
+     <Route path="/about" element={<About />}/>
+     <Route path="/projects" element={<Project />}/>
+     <Route path="/teams" element={<Team />}/>
+
+    
+    </Route>
+  </Routes>
+  </BrowserRouter>
+  
   )
 }
 
